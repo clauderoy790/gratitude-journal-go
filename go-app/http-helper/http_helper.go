07456f1b931f528eabe69603050431e9a0e16c18
Claude Roy <claude.roy@mheducation.com>
2021-08-07 16:25:34 -0500

    GJG-1: start to replace gin with gorilla/mux

--- go-app/http-helper/http_helper.go
+++ go-app/http-helper/http_helper.go
@@ -1,1 +1,24 @@
 package http_helper
+
+import (
+	"encoding/json"
+	"net/http"
+)
+
+type M map[string]interface{}
+
+func WriteJson(w http.ResponseWriter, val interface{}) {
+	data, err := json.Marshal(val)
+	if err != nil {
+		WriteError(w, err, http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(data)
+}
+
+func WriteError(w http.ResponseWriter, err error, errorStatusCode int) {
+	http.Error(w, err.Error(), errorStatusCode)
+}
--- go-app/server/server.go
+++ go-app/server/server.go
@@ -1,1 +1,33 @@
 package server
+
+import (
+	"context"
+	"github.com/clauderoy790/gratitude-journal/config"
+	"github.com/clauderoy790/gratitude-journal/helpers"
+	"github.com/gorilla/mux"
+	"net/http"
+)
+
+type Server struct {
+	mux        *mux.Router
+	ctx        context.Context
+	cfg        *config.Config
+	httpServer *http.Server
+}
+
+func New(ctx context.Context) *Server {
+	server := Server{
+		ctx: ctx,
+	}
+	server.setupRoutes()
+	return &server
+}
+
+func (server *Server) Start() {
+	helpers.MongoHelper.Connect()
+	defer helpers.MongoHelper.Disconnect()
+
+	server.httpServer = &http.Server{
+		Handler: server.mux,
+	}
+}
